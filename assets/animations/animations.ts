9c51311d79d298a9a786fca7ad1e26ca59ad5ea3
Liad-PC <khgs2411@gmail.com>
2019-07-30 00:14:01 +0300

    Auto-generated commit

--- assets/animations/animations.ts
+++ assets/animations/animations.ts
@@ -83,3 +83,17 @@ export let expand = trigger("expand", [
   ]),
   transition("* => void", [animate(500), style({ width: "0%" })])
 ]);
+
+export let navbarToggle = trigger("navbarToggle", [
+  state("open", style({ width: "180px" })),
+  state("closed", style({ width: "0%" })),
+  transition("closed => open", [animate(300), style({ width: "180px" })]),
+  transition("open => closed", [animate(300)]),
+  state("right", style({ transform: "translateX(182px)" })),
+  state("left", style({ transform: "translateX(0%)" })),
+  transition("right => left", [
+    animate(300),
+    style({ transform: "translateX(182px)" })
+  ]),
+  transition("left => right", [animate(300)])
+]);
